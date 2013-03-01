f31046fce7cfa09b74e134cfa0aaf39e427e0b6d
Alexandre Dube <adube@mapgears.com>
2013-03-01 13:25:18 -0500

    custom layer switcher added

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -20,9 +20,17 @@ var app = {
 
     map: null,
 
+    currentBaseLayer: null,
+
+    baseLayers: null,
+
+    overlayLayers: null,
+
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        this.baseLayers = {};
+        this.overlayLayers = {};
     },
     // Bind Event Listeners
     //
@@ -38,6 +46,7 @@ var app = {
     onDeviceReady: function() {
         //app.receivedEvent('deviceready');
         app.createMap();
+        app.manageOrientation();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -52,17 +61,114 @@ var app = {
     },
 
     createMap: function() {
-        // map with default layers
+        // initial base layers
+        app.baseLayers["OSM Standard"] = L.tileLayer(
+            'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+            {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    
+            }
+        );
+        app.baseLayers["OSM Cycle Map"] = L.tileLayer(
+            'http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+            {
+                attribution: 'Tiles courtesy of <a href="http://www.opencyclemap.org/" target="_blank">Andy Allan</a>'
+            }
+        );
+        
         app.map = new L.Map('map', {
             center: new L.LatLng(48.41, -71.09),
             zoom: 13
-        }).addLayer(
-            L.tileLayer(
-                'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-                {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }
-            )
+        });
+
+        // only add the first layer to the layer object, add the others to the
+        $.each(app.baseLayers, function(name, layer) {
+            app.map.addLayer(layer);
+            app.currentBaseLayer = layer;
+            return false;
+        });
+
+        // initialize the layer list
+        app.initLayerList();
+    },
+
+    manageOrientation: function() {
+        $(window).bind(
+            "orientationchange resize pageshow",
+            app.fixContentHeight
         );
+        app.fixContentHeight();
+    },
+
+    fixContentHeight: function() {
+        console.log("ADUBE - fixContentHeight");
+
+        var footer = $("div[data-role='footer']:visible"),
+            content = $("div[data-role='content']:visible:visible"),
+            viewHeight = $(window).height(),
+            contentHeight = viewHeight - footer.outerHeight();
+
+        if ((content.outerHeight() + footer.outerHeight()) !== viewHeight) {
+            contentHeight -= (content.outerHeight() - content.height() + 1);
+            content.height(contentHeight);
+        }
+    },
+
+    initLayerList: function() {
+        $('#layerspage').page();
+        $('<li>', {
+                "data-role": "list-divider",
+                text: "Base Layers"
+            })
+            .appendTo('#layerslist');
+        $.each(app.baseLayers, function(key, layer) {
+            app.addLayerToList(key, layer);
+        });
+
+        $('<li>', {
+                "data-role": "list-divider",
+                text: "Overlay Layers"
+            })
+            .appendTo('#layerslist');
+        $.each(app.overlayLayers, function(key, layer) {
+            app.addLayerToList(key, layer);
+        });
+        $('#layerslist').listview('refresh');
+    },
+
+    addLayerToList: function(name, layer) {
+        var item = $('<li>', {
+                "data-icon": "check",
+                "class": app.map.hasLayer(layer) ? "checked" : ""
+            })
+            .append($('<a />', {
+                text: name
+            })
+                .click(function() {
+                    $.mobile.changePage('#mappage');
+                    if (app.baseLayers[name]) {
+                        app.map.removeLayer(app.currentBaseLayer);
+                        app.map.addLayer(layer);
+                        app.currentBaseLayer = layer;
+                    } else {
+                        // todo...
+                        //layer.setVisibility(!layer.getVisibility());
+                    }
+                })
+            )
+            .appendTo('#layerslist');
+    
+        // todo - support this as well
+        /*
+        layer.events.on({
+            'visibilitychanged': function() {
+                $(item).toggleClass('checked');
+            }
+        });
+        */
+    },
+
+    setBaseLayer: function(name, layer) {
+        
     }
 };
